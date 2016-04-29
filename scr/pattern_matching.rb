aa76361b9d73f6d1de5dc850a086fc342aafc8a9
JorgeLuis <lescanojorgeluis@gmail.com>
2016-04-29 11:56:25 -0300

    usando bloques

--- scr/pattern_matching.rb
+++ scr/pattern_matching.rb
@@ -1,28 +1,11 @@
 class Matcher
 
-  def a_variable_name(valor)
-    self.instance_eval{valor}
-    true
-  end
-
   def val(variable_A, variable_B)
     variable_A.eql? variable_B
   end
   
   def type(objeto,tipo)
     objeto.class.equal?tipo
-  end
-
-  def list (values1,values2,match_size)
-    @flag=true
-    values1.each_index { |index|
-      if values1[index]!=values2[index]
-        @flag=false
-      end}
-    if (values1.length!=values2.length && match_size)
-      @flag=false
-    end
-    @flag
   end
 
   def duck_typing(*args, obj)
