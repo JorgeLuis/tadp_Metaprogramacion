a816cf0655a96482ee2cb3f3a3f8d87af09fa76d
Eric Brian Quinteros Jimenez <eric.brian.quinteros@gmail.com>
2016-05-14 00:08:58 -0300

    Prueba de matchers en object

---
+++ test/borrador_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require_relative '../scr/borrador'
+
+describe 'pruebas de borrador' do
+
+  it 'Matcher 1.a Variable' do
+    expect(:a.call(5)).to eq(true)
+    # expect(a).to eq(5)
+  end
+
+  it 'Matcher 1.b Valor' do
+    expect(val(5).call(5)).to be(true)
+    expect(val('aaa').call('aab')).to eq(false)
+  end
+
+  it 'Test: verifica si un objeto es del tipo indicado' do
+    expect(type(Integer).call 5).to be(true)
+    expect(type(Symbol).call ':5').to be(false)
+    expect(type(Symbol).call (:g)).to be(true)
+  end
+end
